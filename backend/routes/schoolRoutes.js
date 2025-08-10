856da0d1b5cba95af552c1fce4fa24df99ecaa1a
kiran0702 <kiran7722p@gmail.com>
2025-08-10 21:29:22 +0530

    changed the api name

--- backend/routes/schoolRoutes.js
+++ backend/routes/schoolRoutes.js
@@ -4,6 +4,6 @@ import { addSchool, listSchools } from '../controllers/schoolController.js';
 const router = Router();
 
 router.post('/addSchool', addSchool);
-router.get('/listSchools', listSchools);
+router.get('/listSchools', listSchool);
 
 export default router;
